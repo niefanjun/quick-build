715af46332626eff1262c5fc104f7489131b2f1f
niefanjun <niefanjun@gmail.com>
2020-08-03 15:08:09 +0800

    feat(normal): 调整commitizen类型

--- cz-config.js
+++ cz-config.js
@@ -1,22 +1,20 @@
 module.exports = {
 
   types: [
-    {value: '特性',     name: '特性:    一个新的特性'},
-    {value: '修复',      name: '修复:    修复一个Bug'},
-    {value: '文档',     name: '文档:    变更的只有文档'},
-    {value: '格式',    name: '格式:    空格, 分号等格式修复'},
-    {value: '重构', name: '重构:    代码重构，注意和特性、修复区分开'},
-    {value: '性能',     name: '性能:    提升性能'},
-    {value: '测试',     name: '测试:    添加一个测试'},
-    {value: '工具',    name: '工具:    开发工具变动(构建、脚手架工具等)'},
-    {value: '回滚',   name: '回滚:    代码回退'}
+    {value: 'feat',     name: 'feat:   一个新的特性'},
+    {value: 'fix',      name: 'fix:    修复一个Bug'},
+    {value: 'docs',     name: 'docs:    变更的只有文档'},
+    {value: 'style',    name: 'style:    空格, 分号等格式修复'},
+    {value: 'refactor', name: 'refactor:    代码重构，注意和特性、修复区分开'},
+    {value: 'perf',     name: 'perf:    提升性能'},
+    {value: 'test',     name: 'test:    添加一个测试'},
+    {value: 'chore',    name: 'chore:    开发工具变动(构建、脚手架工具等)'},
+    {value: 'WIP',   name: 'WIP:    代码回退'}
   ],
 
   scopes: [
-    {name: '模块1'},
-    {name: '模块2'},
-    {name: '模块3'},
-    {name: '模块4'}
+    {name: 'normal'},
+    {name: 'hotfix'}
   ],
 
   // it needs to match the value for field type. Eg.: 'fix'
